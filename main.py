3ee7289091f424670915c145845ca68edc37126f
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2019-11-29 08:06:03 -0500

    Fixed Linear Regresion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn import svm, linear_model
 import numpy as np
 import csv
@@ -36,4 +37,8 @@ if algorithm == 'SVC':
 elif algorithm == 'linear':
     X_train, X_test, y_train, y_test = train_test_split(all_x, all_y, test_size=0.2, random_state=0)
     regr = linear_model.LinearRegression().fit(X_train, y_train)
-    print('Linear Regression Score:', regr.score(X_test, y_test))
+    y_predict = regr.predict(X_test)
+    # The mean squared error
+    print("Mean squared error: %.2f" % mean_squared_error(y_test, y_predict))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % r2_score(y_test, y_pred))
