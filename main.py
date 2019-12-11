b7ce80b320693ebd14c9c3229f4c4fa95201dacd
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2019-12-10 20:28:48 -0500

    Added MLP

--- main.py
+++ main.py
@@ -190,7 +190,8 @@ elif algorithm == 'tree':
 elif algorithm == 'neural':
     print('You Choose MLP Neural Networks')
     tuned_parameters = [
-
+        #{'hidden_layer_sizes': [(5,3), (100,), (100, 50)], 'activation': ['identity', 'logistic', 'tanh', 'relu'], 'solver': ['lbfgs', 'sgd', 'adam'], 'max_iter': [1000]}
+        {'hidden_layer_sizes': [(100,)], 'activation': ['relu'], 'solver': ['adam'], 'max_iter': [1000]}
     ]
     machine_learning(MLPClassifier(), tuned_parameters)
 elif algorithm == 'bayes':
